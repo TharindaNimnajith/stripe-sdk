28694459f02fa1ffe91a0466bb8df07b23c9f384
Tharinda Nimnajith Rajapaksha <tharindarajapakshe@gmail.com>
2023-06-11 19:42:18 +0530

    init

---
+++ src/main/java/com/example/stripesdk/StripeSdkApplication.java
@@ -0,0 +1,13 @@
+package com.example.stripesdk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StripeSdkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StripeSdkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/stripesdk/StripeSdkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stripesdk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StripeSdkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
